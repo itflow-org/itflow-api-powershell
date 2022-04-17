513076582388da4f82233c2f66cc9724363580da
Marcus Hill <bcf8c9f2@opayq.com>
2022-04-17 21:58:00 +0100

    Add asset create/read

---
+++ assets/create.ps1
@@ -0,0 +1,47 @@
+﻿# ITFlow API - PowerShell examples
+# https://itflow.org
+
+# Create asset(s)
+
+# API Key
+$apiKey = "a1wEoh1F3omPhea6"
+
+# Base site URL
+$siteUrl = "https://demo.itflow.org"
+
+# Data
+# All attributes must be specified, even if left blank
+$body = @"
+{
+    "api_key" : "$apiKey",
+    "asset_name" : "My Laptop 2",
+    "asset_type" : "Laptop",
+    "asset_make" : "Dell",
+    "asset_model" : "Optiplex",
+    "asset_serial" : "XYZ",
+    "asset_os" : "Windows 10",
+    "asset_ip" : "192.168.10.10",
+    "asset_mac" : "",
+    "asset_purchase_date" : "",
+    "asset_warranty_expire" : "2024-02-01",
+    "asset_install_date" : "",
+    "asset_notes" : "This is a cool laptop!",
+    "asset_meshcentral_id" : "",
+    "asset_vendor_id" : "",
+    "asset_location_id" : "",
+    "asset_contact_id" : "",
+    "asset_network_id" : "16",
+    "client_id" : "10"
+}
+"@
+
+# Module / Endpoint
+$module = "/api/v1/assets/create.php"
+
+# Build URI from defined data
+$uri = $siteUrl + $module
+
+# Request
+# Use Invoke-WebMethod instead to see more info about the request/response
+Invoke-RestMethod -Method Post -Uri $uri -Body $body
+
---
+++ assets/read.ps1
@@ -0,0 +1,38 @@
+﻿# ITFlow API - PowerShell examples
+# https://itflow.org
+
+# Read assets
+
+# API Key
+$apiKey = "a1wEoh1F3omPhea6"
+
+# Base site URL
+$siteUrl = "https://demo.itflow.org"
+
+# Module / Endpoint
+$module = "/api/v1/assets/read.php"
+
+# Build URI from defined data
+# Will return all assets by default (single client/all clients depends on API key scope)
+$uri = $siteUrl + $module + "?api_key=" + $apiKey
+
+
+# Other URL examples
+
+# All assets for a specific client ID
+# $uri = $siteUrl + $module + "?api_key=" + $apiKey + "&client_id=10"
+
+# All assets of a specific type
+# $uri = $siteUrl + $module + "?api_key=" + $apiKey + "&asset_type=Laptop"
+
+# Specific asset ID
+# $uri = $siteUrl + $module + "?api_key=" + $apiKey + "&asset_id=22"
+
+
+# Specific asset name
+# $uri = $siteUrl + $module + "?api_key=" + $apiKey + "&asset_name=SRV01"
+
+
+# Request
+# Use Invoke-WebMethod instead to see more info about the request/response
+Invoke-RestMethod -Method GET -Uri $uri
