54a8f82c266471ab2fcf2b61ad50171b0ed43b48
wrongecho <32306651+wrongecho@users.noreply.github.com>
2024-08-25 14:22:25 +0100

    Create read.ps1
    Add info on how to use credentials/logins API

---
+++ credentials/read.ps1
@@ -0,0 +1,27 @@
+# ITFlow API - PowerShell examples
+# https://itflow.org
+
+# Read credentials (originally called "logins")
+
+# API Key
+$apiKey = "a1wEoh1F3omPhea6"
+
+# Credential decryption key
+$credKey = "JKyA0daoyOv7oKpw"
+
+# Base site URL
+$siteUrl = "https://demo.itflow.org"
+
+# Module / Endpoint
+$module = "/api/v1/credentials/read.php"
+
+# Build URI from defined data
+# Will return all assets by default (single client/all clients depends on API key scope)
+$uri = $siteUrl + $module + "?api_key=" + $apiKey + "&api_key_decrypt_password=" + $credKey
+
+# Another URL example: Specific credential/login ID
+#$uri = $siteUrl + $module + "?api_key=" + $apiKey + "&api_key_decrypt_password=" + $credKey + "&login_id=7"
+
+# Request
+# Use Invoke-WebMethod instead to see more info about the request/response
+$a = Invoke-RestMethod -Method GET -Uri $uri
