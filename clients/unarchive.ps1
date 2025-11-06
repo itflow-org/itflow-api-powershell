2dfbfbaf0e21a620eb62319a45ed837ad49c2261
wrongecho <32306651+wrongecho@users.noreply.github.com>
2025-11-06 15:20:26 +0000

    Create unarchive.ps1

---
+++ clients/unarchive.ps1
@@ -0,0 +1,28 @@
+# ITFlow API - PowerShell examples
+# https://itflow.org
+
+# Un-Archive client(s)
+
+# API Key
+$apiKey = "05438411f4fd492f"
+
+# Base site URL
+$siteUrl = "https://demo.itflow.org"
+
+# Data
+$body = @"
+{
+    "api_key" : "$apiKey",
+    "client_id" : "1017"
+}
+"@
+
+# Module / Endpoint
+$module = "/api/v1/clients/unarchive.php"
+
+# Build URI from defined data
+$uri = $siteUrl + $module
+
+# Request
+# Use Invoke-WebRequest instead to see more info about the request/response
+Invoke-RestMethod -Method Post -Uri $uri -Body $body
