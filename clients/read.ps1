86c5700d31aaa120a52baadee5b4bb03b4f6357e
wrongecho <32306651+wrongecho@users.noreply.github.com>
2026-04-09 14:57:23 +0100

    Add example for client read endpoint

---
+++ clients/read.ps1
@@ -0,0 +1,26 @@
+﻿# ITFlow API - PowerShell examples
+# https://itflow.org
+
+# Read clients
+
+# API Key
+$apiKey = "a1wEoh1F3omPhea6"
+
+# Base site URL
+$siteUrl = "https://demo.itflow.org"
+
+# Module / Endpoint
+$module = "/api/v1/clients/read.php"
+
+# Build URI from defined data
+# Will return all clients by default
+$uri = $siteUrl + $module + "?api_key=" + $apiKey
+
+# Other URL examples
+
+# Specific client name
+$uri = $siteUrl + $module + "?api_key=" + $apiKey + "&client_name=ExampleOrg"
+
+# Request
+# Use Invoke-WebMethod instead to see more info about the request/response
+Invoke-RestMethod -Method GET -Uri $uri
